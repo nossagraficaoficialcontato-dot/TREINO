7bb3941dea67943e3ec40d6d0c0983961acb7bd3
nossagraficaoficialcontato-dot <nossagraficaoficial.contato@gmail.com>
2026-08-22 06:52:02 -0300

    FITPRO V7 config

---
+++ js/config.js
@@ -0,0 +1,3 @@
+export const SUPABASE_URL='https://ciqzrrpsnhbsqafpbdsu.supabase.co';
+export const SUPABASE_KEY='sb_publishable_L_KJL0I0Xzu9Mdt8mYGPVw_LvbjICBM';
+export const db=window.supabase.createClient(SUPABASE_URL,SUPABASE_KEY);
